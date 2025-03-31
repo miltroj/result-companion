1a6f513dc40b8cf4efdb6987c35d469001fc5121
Milosz <mil.troj@gmail.com>
2025-03-31 22:35:54 +0200

    ollama auto installation in place

--- result_companion/core/analizers/local/ollama_runner.py
+++ result_companion/core/analizers/local/ollama_runner.py
@@ -1,4 +1,5 @@
 import subprocess
+from sys import platform
 
 from result_companion.core.utils.logging_config import logger
 
@@ -23,6 +24,56 @@ def check_model_installed(
         logger.debug(f"Model {model_name} is installed.")
     except Exception as e:
         raise Exception(f"Failed to check if model is installed: {e}")
+
+
+def auto_install_ollama(
+    brew_installation_cmd: list = ["brew", "install", "ollama"],
+    linux_update_cmd: list = ["sudo", "apt-get", "update"],
+    linux_install_cmd: list = ["sudo", "apt-get", "install", "-y", "ollama"],
+    ollama_version: list = ["ollama", "--version"],
+) -> bool:
+    """
+    Check if Ollama is installed; if not, install it automatically.
+    Returns None if installation is successful, or raises an Exception.
+    """
+    try:
+        check_ollama_installed(ollama_version=ollama_version)
+        logger.info("Ollama is already installed.")
+        return True
+    except Exception:
+        logger.warning("Ollama not found. Attempting to install...")
+
+    if platform == "darwin":
+        try:
+            logger.info("Ollama not found. Installing via Homebrew...")
+            subprocess.run(brew_installation_cmd, check=True)
+        except Exception as e:
+            raise Exception(
+                "Automatic installation via Homebrew failed. Please install Ollama manually."
+            ) from e
+
+    elif str(platform).startswith("linux"):
+        try:
+            logger.info("Ollama not found. Installing via apt-get...")
+            subprocess.run(linux_update_cmd, check=True)
+            subprocess.run(linux_install_cmd, check=True)
+        except Exception as e:
+            raise Exception(
+                "Automatic installation via apt-get failed. Please install Ollama manually."
+            ) from e
+
+    else:
+        raise Exception(
+            "Automatic installation is not supported on your OS. Please install Ollama manually."
+        )
+
+    try:
+        check_ollama_installed(ollama_version=ollama_version)
+    except Exception as e:
+        raise Exception("Ollama installation did not complete successfully.") from e
+
+    logger.info("Ollama installed successfully!")
+    return True
 
 
 def ollama_on_init_strategy(model_name: str, *args, **kwargs) -> None:
--- tests/unittests/core/analizers/local/test_ollama_runner.py
+++ tests/unittests/core/analizers/local/test_ollama_runner.py
@@ -4,6 +4,7 @@ from unittest.mock import patch
 import pytest
 
 from result_companion.core.analizers.local.ollama_runner import (
+    auto_install_ollama,
     check_model_installed,
     check_ollama_installed,
     ollama_on_init_strategy,
@@ -48,3 +49,54 @@ def test_ollama_on_init_strategy(mock_ollama_installed, mock_model_installed) ->
     assert ollama_on_init_strategy(model_name="llama31") is None
     mock_ollama_installed.assert_called_once()
     mock_model_installed.assert_called_once_with(model_name="llama31")
+
+
+def test_auto_install_ollama_on_mac() -> None:
+    with patch(
+        "result_companion.core.analizers.local.ollama_runner.platform",
+        return_value="darwin",
+    ):
+        assert auto_install_ollama(
+            brew_installation_cmd=["echo", "'Ollama installed!'"],
+            ollama_version=["echo", "'Installed!'"],
+        )
+
+
+def test_auto_install_ollama_on_linux() -> None:
+    with patch(
+        "result_companion.core.analizers.local.ollama_runner.platform",
+        return_value="linux",
+    ):
+        assert auto_install_ollama(
+            linux_update_cmd=["echo", "'Ollama updated!'"],
+            linux_install_cmd=["echo", "'Ollama installed!'"],
+            ollama_version=["echo", "'Installed!'"],
+        )
+
+
+def test_auto_install_ollama_on_other_os() -> None:
+    with patch(
+        "result_companion.core.analizers.local.ollama_runner.platform",
+    ) as mock_platform:
+        mock_platform.return_value = "other_os"
+        with pytest.raises(
+            Exception,
+            match="Automatic installation is not supported on your OS. Please install Ollama manually.",
+        ):
+            auto_install_ollama(
+                ollama_version=["exit", "1"],
+            )
+
+
+def test_auto_install_ollama_installation_fails() -> None:
+
+    with pytest.raises(
+        Exception,
+        match="Ollama installation did not complete successfully.",
+    ):
+        auto_install_ollama(
+            brew_installation_cmd=["echo", "'Ollama installing!'"],
+            linux_update_cmd=["echo", "'linux updated!'"],
+            linux_install_cmd=["echo", "'Ollama installing!'"],
+            ollama_version=["exit", "1"],
+        )
